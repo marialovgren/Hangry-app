6c6e9e3715c452ab25e483a1a89602eda2262c7d
JonaTorsson <jona.torsson@gmail.com>
2022-09-19 11:33:44 +0200

    setup signup i context

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,4 +1,9 @@
 import { createContext, useContext } from 'react'
+import { doc, setDoc } from 'firebase/firestore'
+import { ref, getDowloadURL, uploadeBytes } from 'firebase/storage'
+import { auth, storage, db } from '../firebase'
+import { useState } from 'react'
+
 
 const AuthContext = createContext()
 
@@ -6,12 +11,39 @@ const useAuthContext = () => {
 	return useContext(AuthContext)
 }
 
-const AuthContextProvider = () => {
 
+const AuthContextProvider = ({ children }) => {
+	const [ currentUser, setCurrentUser ] = useState(null)
+	const [ loading, setLoading ] = useState(true)
+
+	const signup = async (email, password, name, photo) => {
+		await createdUserWithEmailAndPassword(auth, email, password)
+
+		await setDisplayNameAndPhoto(name, photo)
+
+		await reloadUser()
+
+		const docRef = doc(db, 'users', auth.currentUser.uid)
+		await setDoc(docRef, {
+			name,
+			email,
+			photoURL: auth.currentUser.photoURL,
+			admin,
+		})
+	}
+
+	const contextValues = {
+		currentUser,
+		signup,
+	}
     return (
-        <div>
-            
-        </div>
+		<AuthContext.Provider value={contextValues}>
+			{loading ? (
+				<p>Loading...</p>
+			) : (
+				children
+			)}
+		</AuthContext.Provider>
     )
 }
 
