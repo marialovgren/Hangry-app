a92400a3f3616e87855509ed3eb18ad307603871
jo-oo <johannakoreo@gmail.com>
2022-10-05 20:07:33 +0200

    added Orderby to query

--- src/hooks/useGetQueryRestaurants.jsx
+++ src/hooks/useGetQueryRestaurants.jsx
@@ -1,19 +1,36 @@
-import useStreamCollection from "./useStreamCollection"
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { 
+        collection, 
+        query, 
+        where, 
+        orderBy } from "firebase/firestore";
 import { db } from '../firebase'
 import { useFirestoreQueryData } from '@react-query-firebase/firestore'
 
-const useGetQueryRestaurants = (queryCity) => {
+const useGetQueryRestaurants = (querys) => {
 
+    console.log("querys" , querys)
+    console.log("nameOrder" , querys.nameOrder)
+    //get rererence of the collection from data base (Firestore)
     const collectionRef = collection(db, 'restaurants')
 
-    const queryKey = ['restaurants', queryCity]
+    const queryKey = ['restaurants', querys]
 
     let queryRef 
 
-    if (queryCity.city) {
-        queryRef = query(collectionRef, where('restaurantCity', '==', queryCity.city))
+    //Gets all wuerys in choosen city
+    if (querys.city) {
+
+        //if user didnt filter on neither type nor offer
+        //if (querys.type === 'no-filter' && querys.offer === 'no-filter') {
+
+             //User searched for/used position of City only. Show all restauarnts in choosen city
+             console.log('querys.nameOrder',querys.nameOrder)
+             queryRef = query(collectionRef, where('restaurantCity', '==', querys.city), orderBy('restaurantName', querys.nameOrder))
+        
+        //}
+  
     }
+
 
     const restaurantQuery = useFirestoreQueryData(queryKey, queryRef, {
         idField: 'id',
