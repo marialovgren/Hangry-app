91373f593a3346233bbbb710157b43cad8088cfc
JonaTorsson <jona.torsson@gmail.com>
2022-10-05 20:38:32 +0200

    solved conflicts

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,143 +2,110 @@
 import React, { useCallback, useState, useRef, useEffect } from 'react'
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
 import { useSearchParams } from 'react-router-dom'
-
 /** API-service **/
 import mapAPI from '../services/mapAPI'
-
 /** Hooks **/
 import useGetAllRestaurants from '../hooks/useGetAllRestaurants'
-
 /** Components **/
 import Sidebar from './Sidebar'
-
 const libraries = ['places'] 
-
 const containerStyle = {
-	width: '100vw',
-	height: '100vh'
+    width: '100vw',
+    height: '100vh'
 }
-
 const Map = () => {
-
-  	const { isLoaded } = useLoadScript({
-		/* id: 'google-map-script', */ // behövs denns????
-		googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-		libraries, 
-	})
-
-	// const [ searchParams, setSearchParams ] = useSearchParams()
-  	const [map, setMap] = useState(/** @type google.maps.Map */ (null))
-	const [userPosition, setUserPosition] = useState({lat: 55.6050, lng: 13.0038})
-	const { data: restaurants } = useGetAllRestaurants()  
-	const { selectedRestaurant, setSelectedRestaurant } = useState(null)
-	const [searchParams, setSeachParams] = useSearchParams()
-
-	//const [ userLocation, setUserLocation ] = useState("")
-	/* const [city, setCity] = useState(null)
-	const [queryCity, setQueryCity] = useState({
+    const { isLoaded } = useLoadScript({
+        /* id: 'google-map-script', */ // behövs denns????
+        googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+        libraries, 
+    })
+    const [map, setMap] = useState(/** @type google.maps.Map */ (null))
+    const [userPosition, setUserPosition] = useState({lat: 55.6050, lng: 13.0038})
+    const { data: restaurants } = useGetAllRestaurants()  
+    const { selectedRestaurant, setSelectedRestaurant } = useState(null)
+    const [searchParams, setSeachParams] = useSearchParams()
+    //const [ userLocation, setUserLocation ] = useState("")
+    /* const [city, setCity] = useState(null)
+    const [queryCity, setQueryCity] = useState({
         city,
     }) */
-	
-	/** Moves map to the restaurant that user clicked on **/
-	const handleRestaurantItemClick = (place) => {
-		setSelectedRestaurant(place)
-		map.panTo(place.coords)
-	}
-
-	/** Handles what will happen when user have submitted searchform **/
-	const handleOnSubmit = async (address) => {
-		if (!address) {
-			return
-		}
-		
-		// get the coordinates for the place that user searched for
-		const coordinates = await mapAPI.getLatAndLong(address) 
-		map.panTo(coordinates) // moves map view to the chosen place
-		console.log("coordinates to the place you searched for", coordinates)
-		setUserPosition(coordinates) // sets userPosition to same value as the coordinates from searchfield
-
-		setSeachParams({city: await mapAPI.getSearchedCity(coordinates)})		
-	}
-		getUserPosition()
-		
+    /** Moves map to the restaurant that user clicked on **/
+    const handleRestaurantItemClick = (place) => {
+        setSelectedRestaurant(place)
+        map.panTo(place.coords)
+    }
+    /** Handles what will happen when user have submitted searchform **/
+    const handleOnSubmit = async (address) => {
+        if (!address) {
+            return
+        }
+        // get the coordinates for the place that user searched for
+        const coordinates = await mapAPI.getLatAndLong(address) 
+        map.panTo(coordinates) // moves map view to the chosen place
+        console.log("coordinates to the place you searched for", coordinates)
+        setUserPosition(coordinates) // sets userPosition to same value as the coordinates from searchfield
+        setSeachParams({city: await mapAPI.getSearchedCity(coordinates)})       
+    }
+    useEffect(() => {
+        const getUserPosition = async () => {
+            if (searchParams.get('city')) {
+                setUserPosition(await mapAPI.getLatAndLong(searchParams.get('city')))
+            } else if('geolocation' in navigator) {
+                navigator.geolocation.getCurrentPosition((position) => {
+                    setUserPosition({ lat: position.coords.latitude, lng: position.coords.longitude })
+                })
+            }
+        }
+        getUserPosition()
     }, [searchParams]) 
-
-	useEffect(() => {
-
-		const getUserPosition = async () => {
-			if (searchParams.get('city')) {
-				setUserPosition(await mapAPI.getLatAndLong(searchParams.get('city')))
-			} else if('geolocation' in navigator) {
-				navigator.geolocation.getCurrentPosition((position) => {
-					setUserPosition({ lat: position.coords.latitude, lng: position.coords.longitude })
-				})
-			}
-		}
-		getUserPosition()
-		
-    }, [searchParams]) 
-
-
-	//const mapRef = useRef()
-
-	/* const onMapLoad = useCallback((map) => {
-		mapRef.current = map
-	}, []) */
-
-	/* const panToLocation = useCallback(({ lat, lng }) => {
-		setUserLocation({ lat, lng })
-		mapRef.current.panTo({ lat, lng })
-		mapRef.current.setZoom(15)
-		console.log("latitud:", lat + "longitud:", lng)
-	}, []) */
-
-	/* const onUnmount = React.useCallback(function callback(map) {
-		setMap(null)
-	}, []) */
-
-	return isLoaded ? (
-		<>
-			<div className="mapBox">
-				<GoogleMap
-					mapContainerStyle={containerStyle}
-					center={userPosition}
-					zoom={15}
-					onLoad={map => setMap(map)}
-					/* onUnmount={onUnmount} */
-					options={{
-						mapTypeId: 'roadmap', //set default page to show Roadmap. It does already but this is our setting
-						mapTypeControl:false, //removes Sattelite and Terrain Option Buttons
-					}}
-				>
-					{ /* Child components, such as markers, info windows, etc. */ }
-
-					<Marker 
-						position={userPosition}
-						// onClick={handleUserMarkerOnClick}
-					/>
-
-					{restaurants && restaurants.map((place, index) => (
-						<Marker 
-							key={index}
-							position={place.coordinates}
-						/>
-					))}
-
-					{/* {userLocation && (
-						<Marker 
-							position={{ lat: userLocation.lat, lng: userLocation.lng }} />
-						)} */}
-					<></>
-
-				</GoogleMap>
-			</div>
-
-			<Sidebar onSubmit={handleOnSubmit} /* myLocation={panToLocation} */ /* city={city} setCity={setCity}  */restaurants={restaurants} />
-
-		</>
-	) 
-		: <></>
+    //const mapRef = useRef()
+    /* const onMapLoad = useCallback((map) => {
+        mapRef.current = map
+    }, []) */
+    /* const panToLocation = useCallback(({ lat, lng }) => {
+        setUserLocation({ lat, lng })
+        mapRef.current.panTo({ lat, lng })
+        mapRef.current.setZoom(15)
+        console.log("latitud:", lat + "longitud:", lng)
+    }, []) */
+    /* const onUnmount = React.useCallback(function callback(map) {
+        setMap(null)
+    }, []) */
+    return isLoaded ? (
+        <>
+            <div className="mapBox">
+                <GoogleMap
+                    mapContainerStyle={containerStyle}
+                    center={userPosition}
+                    zoom={15}
+                    onLoad={map => setMap(map)}
+                    /* onUnmount={onUnmount} */
+                    options={{
+                        mapTypeId: 'roadmap', //set default page to show Roadmap. It does already but this is our setting
+                        mapTypeControl:false, //removes Sattelite and Terrain Option Buttons
+                    }}
+                >
+                    { /* Child components, such as markers, info windows, etc. */ }
+                    <Marker 
+                        position={userPosition}
+                        // onClick={handleUserMarkerOnClick}
+                    />
+                    {restaurants && restaurants.map((place, index) => (
+                        <Marker 
+                            key={index}
+                            position={place.coordinates}
+                        />
+                    ))}
+                    {/* {userLocation && (
+                        <Marker 
+                            position={{ lat: userLocation.lat, lng: userLocation.lng }} />
+                        )} */}
+                    <></>
+                </GoogleMap>
+            </div>
+            <Sidebar onSubmit={handleOnSubmit} /* myLocation={panToLocation} */ /* city={city} setCity={setCity}  */restaurants={restaurants} />
+        </>
+    ) 
+        : <></>
 }
-
 export default React.memo(Map)
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,34 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import GetMyLocation from './GetMyLocation'
 import SearchField from './SearchField'
 import ResultsList from './ResultsList'
 import { ListGroup, Container, Button, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
-import mapAPI from '../services/mapAPI'
-import { faLocationArrow } from '@fortawesome/free-solid-svg-icons'
-
-const Sidebar = ({onSubmit, userPosition, city, setCity, restaurants}) => {
+const Sidebar = ({onSubmit, myLocation, city, setCity, restaurants}) => {
     const [open, setOpen] = useState(false)
-
     const resetCity = () => {
-		setCity(null)
-		setOpen(false)
-	}
-
-	const [ cityWhere, setCityWhere ] = useState('Malmö')
-	// const [ queryLimits, setQueryLimits ] = useState({
-	// 	cityWhere,
-	// })
-
-
-	useEffect(() => {
-		const changeCityWhere = async () => {
-			setCityWhere(await mapAPI.getSearchedCity(userPosition))
-		}
-		changeCityWhere()
-	}, [userPosition])
-
+        setCity(null)
+        setOpen(false)
+    }
     return (
         <>
         {/* Mobilversion */}
@@ -37,56 +19,126 @@ const Sidebar = ({onSubmit, userPosition, city, setCity, restaurants}) => {
                     <Col xs={12}>
                         <div className="searchBox d-flex flex-row align-items-center">
                             <SearchField onSubmit={onSubmit} setOpen={setOpen}/>
-                            {/* <GetMyLocation  myLocation={myLocation} /> */}
-							
-							{ cityWhere && (<>
-								<Button
-								type="submit" size="sm" variant="light"
-								className="locateme-position border"
-							>
-								{cityWhere}
-								<FontAwesomeIcon icon={faLocationArrow} />
-							</Button>
+                            <GetMyLocation  myLocation={myLocation} />
                             <Button variant='light' className="py-1 mx-2">
                                 <FontAwesomeIcon icon={faXmark} onClick={resetCity}  />
-                            </Button></>
-							)}
-							
-							
+                            </Button>
                         </div>
                     </Col>
-                    
                     {open && <ResultsList city={city} setCity={setCity} restaurants={restaurants} /> }
                 </Row>
             </div>
-
             {/* Desktopversion */}
             <div className="searchBoxWrapper p-2 d-none d-md-block">
                 <Row>
                     <Col xs={12}>
                         <div className="searchBox d-flex flex-row align-items-center">
                             <SearchField onSubmit={onSubmit} setOpen={setOpen}/>
-                            {/* <GetMyLocation  myLocation={myLocation} /> */}
-							{ cityWhere && (<>
-								<Button
-								type="submit" size="sm" variant="light"
-								className="locateme-position border"
-							>
-								{cityWhere}
-								<FontAwesomeIcon icon={faLocationArrow} />
-							</Button>
+                            <GetMyLocation  myLocation={myLocation} />
                             <Button variant='light' className="py-1 mx-2">
                                 <FontAwesomeIcon icon={faXmark} onClick={resetCity}  />
-                            </Button></>
-							)}
+                            </Button>
                         </div>
                     </Col>
-                    
-                    {open && <ResultsList city={city} setCity={setCity} restaurants={restaurants}  /> }
+                    {open && <ResultsList city={city} setCity={setCity} restaurants={restaurants} /> }
                 </Row>
             </div>
         </>
     )
 }
-
 export default Sidebar
+
+
+
+// import { useState, useEffect } from 'react'
+// import GetMyLocation from './GetMyLocation'
+// import SearchField from './SearchField'
+// import ResultsList from './ResultsList'
+// import { ListGroup, Container, Button, Row, Col } from 'react-bootstrap';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faXmark } from '@fortawesome/free-solid-svg-icons'
+// import mapAPI from '../services/mapAPI'
+// import { faLocationArrow } from '@fortawesome/free-solid-svg-icons'
+
+// const Sidebar = ({onSubmit, userPosition, city, setCity, restaurants}) => {
+//     const [open, setOpen] = useState(false)
+
+//     const resetCity = () => {
+// 		setCity(null)
+// 		setOpen(false)
+// 	}
+
+// 	// const [ cityWhere, setCityWhere ] = useState('Malmö')
+// 	// const [ queryLimits, setQueryLimits ] = useState({
+// 	// 	cityWhere,
+// 	// })
+
+
+// 	// useEffect(() => {
+// 	// 	const changeCityWhere = async () => {
+// 	// 		setCityWhere(await mapAPI.getSearchedCity(userPosition))
+// 	// 	}
+// 	// 	changeCityWhere()
+// 	// }, [userPosition])
+
+//     return (
+//         <>
+//         {/* Mobilversion */}
+//             <div className="searchBoxWrapperMobile p-2 d-md-none">
+//                 <Row>
+//                     <Col xs={12}>
+//                         <div className="searchBox d-flex flex-row align-items-center">
+//                             <SearchField onSubmit={onSubmit} setOpen={setOpen}/>
+//                             {/* <GetMyLocation  myLocation={myLocation} /> */}
+							
+// 							{ cityWhere && (<>
+// 								<Button
+// 								type="submit" size="sm" variant="light"
+// 								className="locateme-position border"
+// 							>
+// 								{cityWhere}
+// 								<FontAwesomeIcon icon={faLocationArrow} />
+// 							</Button>
+//                             <Button variant='light' className="py-1 mx-2">
+//                                 <FontAwesomeIcon icon={faXmark} onClick={resetCity}  />
+//                             </Button></>
+// 							)}
+							
+							
+//                         </div>
+//                     </Col>
+                    
+//                     {open && <ResultsList city={city} setCity={setCity} restaurants={restaurants} /> }
+//                 </Row>
+//             </div>
+
+//             {/* Desktopversion */}
+//             <div className="searchBoxWrapper p-2 d-none d-md-block">
+//                 <Row>
+//                     <Col xs={12}>
+//                         <div className="searchBox d-flex flex-row align-items-center">
+//                             <SearchField onSubmit={onSubmit} setOpen={setOpen}/>
+//                             {/* <GetMyLocation  myLocation={myLocation} /> */}
+// 							{ cityWhere && (<>
+// 								<Button
+// 								type="submit" size="sm" variant="light"
+// 								className="locateme-position border"
+// 							>
+// 								{cityWhere}
+// 								<FontAwesomeIcon icon={faLocationArrow} />
+// 							</Button>
+//                             <Button variant='light' className="py-1 mx-2">
+//                                 <FontAwesomeIcon icon={faXmark} onClick={resetCity}  />
+//                             </Button></>
+// 							)}
+//                         </div>
+//                     </Col>
+                    
+//                     {open && <ResultsList city={city} setCity={setCity} restaurants={restaurants}  /> }
+//                 </Row>
+//             </div>
+//         </>
+//     )
+// }
+
+// export default Sidebar
