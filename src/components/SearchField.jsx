c8a0cdc60355232c5a0126aaa30d4824fa48e3c2
jo-oo <johannakoreo@gmail.com>
2022-10-06 12:21:58 +0200

    out commented everything that opens form

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -4,7 +4,7 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { useRef, useState } from 'react'
 import { Autocomplete } from '@react-google-maps/api'
 
-const SearchField = ({ onSubmit, setOpen }) => {
+const SearchField = ({ onSubmit, setOpen, setQuerys }) => {
     const searchRef = useRef()
 
     const handleFormSubmit = (e) => {
@@ -19,17 +19,20 @@ const SearchField = ({ onSubmit, setOpen }) => {
 
         console.log("The place you chose is: ", searchRef.current.value)
 
-        setOpen(true)
+        //setOpen(true)
+      
 
         searchRef.current.value = ''
     }
 
+    console.log("our setQuery is the choosen thing " + setQuerys?.city)
 	return (
         <>
             <Form onSubmit={handleFormSubmit}  className="d-flex flex-row rounded" >
-                <Form.Group controlId='city'>
+                <Form.Group controlId='address'> {/*väljer själva namn. ej kopplat till db */}
                     <Autocomplete>  
                         <Form.Control
+                        	//onChange={e => setSearchInput(e.target.value)}
                             size="sm"
                             type="search"
                             placeholder="Hungrig?"
@@ -45,9 +48,9 @@ const SearchField = ({ onSubmit, setOpen }) => {
                     size="sm" 
                     variant="light" 
                     className="border"
-                    onClick={() => setOpen(!open)}
+                   /*  onClick={() => setOpen(!open)}
                     aria-controls="example-collapse-text"
-                    aria-expanded={open}
+                    aria-expanded={open} */
                 >
                     <FontAwesomeIcon icon={faMagnifyingGlass} />
                 </Button>
