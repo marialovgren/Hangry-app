045aa3afd683a391822711520cf198effe214cda
jo-oo <johannakoreo@gmail.com>
2022-10-06 12:19:51 +0200

    added back query filtering type option

--- src/hooks/useGetQueryRestaurants.js
+++ src/hooks/useGetQueryRestaurants.js
@@ -9,28 +9,22 @@ import { useFirestoreQueryData } from '@react-query-firebase/firestore'
 const useGetQueryRestaurants = (querys) => {
 
     console.log("querys" , querys)
-    console.log("nameOrder" , querys.nameOrder)
+   
     //get rererence of the collection from data base (Firestore)
     const collectionRef = collection(db, 'restaurants')
 
     const queryKey = ['restaurants', querys]
 
-    let queryRef 
+    //initializes queryRef
+    let queryRef
 
-    //Gets all wuerys in choosen city
-    if (querys.city) {
+        //if user didnt filter on neither type nor offer (no offer options boxes yet in form..)
+        if (querys.type === 'no-filter' && querys.offer === 'no-filter') {
 
-        //if user didnt filter on neither type nor offer
-        //if (querys.type === 'no-filter' && querys.offer === 'no-filter') {
-
-             //User searched for/used position of City only. Show all restauarnts in choosen city
+             //User searched for/used position of City only. Show all restaurants in choosen city
              console.log('querys.nameOrder',querys.nameOrder)
              queryRef = query(collectionRef, where('restaurantCity', '==', querys.city), orderBy('restaurantName', querys.nameOrder))
-        
-        //}
-  
     }
-
 
     const restaurantQuery = useFirestoreQueryData(queryKey, queryRef, {
         idField: 'id',
