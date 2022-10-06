8cf70841e1a275fd9d5c0e9e8e3a7dc174d097ca
jo-oo <johannakoreo@gmail.com>
2022-10-06 09:13:33 +0200

    removed unused functions

--- src/services/mapAPI.js
+++ src/services/mapAPI.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
+
 axios.defaults.baseURL = 'https://maps.googleapis.com/maps/api'
 const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+
 //GET coordinates from an address
 const getLatAndLong = async (address) => {//paramter is address {string} 
     //GET data about address from GeoCode API
@@ -10,8 +12,8 @@ const getLatAndLong = async (address) => {//paramter is address {string}
     //returns object with coordinate values
     return coordinates
 }
-// Get towns name adress
 
+// Get Citys name & adress
 const getSearchedCity = async (coords) => {
 	const res = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${googleMapsApiKey}`)
 
@@ -21,29 +23,10 @@ const getSearchedCity = async (coords) => {
 
 	return cityInfoArr[0].long_name
 }
-const getLocationWithLatLng = async (lat, lon) => {
-    const res = await axios.get(`/json?latlng=${`${lat},${lon}`}&key=${googleMapsApiKey}`)
-    return res.data
-}
-//GET details
-const getDetails = async (place) => {
-    //GET details about a place from Javascript API
-    const response = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${place}&key=${googleMapsApiKey}`)
-    //const details = 
-    //return details;
-}
-//GET Places
-const getPlaces = async () => {
-    //GET from Places API
-    const res = await axios.get(`https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}&libraries=places&callback=initMap`)
-    //const places = 
-    //return places;
-}
+
 const exports = {
     getLatAndLong, 
-    getDetails,
-    getPlaces,
-    getLocationWithLatLng,
     getSearchedCity,
 }
+
 export default exports
